121ec265348648d8c1f3c403814dd9f29ff2b87c
Alexandr Samsonov <a.d.samsonov@yandex.ru>
2017-11-01 16:54:03 +0400

    Настройка сборки

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp'),
+    less = require('gulp-less');
+
+gulp.task('default', function() {
+  gulp.src('app/less/style.less')
+    .pipe(less())
+    .pipe(gulp.dest('app/css'));
+});
